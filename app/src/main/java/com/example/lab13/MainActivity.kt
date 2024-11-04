2d44331089aca35e1ea6ba4085022fec5b6ee0cb
deyson34 <deyson.flores@tecsup.edu.pe>
2024-11-04 16:09:35 -0500

    Animacion con AnimatedVisibility

---
+++ app/src/main/java/com/example/lab13/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.lab13
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab13.ui.theme.Lab13Theme
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab13Theme {
+                MeVesYNoMeVes()
+            }
+        }
+    }
+}
+
+@Composable
+fun MeVesYNoMeVes() {
+    var isVisible by remember { mutableStateOf(false) }
+    var boxColor by remember { mutableStateOf(Color.Blue) }
+
+    // Cambia el color cada segundo
+    LaunchedEffect(Unit) {
+        while (true) {
+            boxColor = Color(
+                red = Random.nextFloat(),
+                green = Random.nextFloat(),
+                blue = Random.nextFloat()
+            )
+            delay(1000L) // Cambia cada 1000 ms (1 segundo)
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 200.dp)
+    ) {
+        Button(onClick = { isVisible = !isVisible }) {
+            Text("Box multicolor")
+        }
+
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(boxColor)
+            )
+        }
+    }
+}
